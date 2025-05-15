dfe5ed9a32d42925792943c4f0b6b5feb3148e88
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-05-15 14:54:00 +0200

    removing MongoDB credentials, to not hardcode them in source files, to secure connection

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const MONGO_URI =
-  'mongodb+srv://AlexR:jadsIc-qimjed-4ruhzu@restful-api-db.bqsosmv.mongodb.net/?retryWrites=true&w=majority&appName=restful-api-db';
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connexion à MongoDB réussie !');
   } catch (err) {
     console.error('Connexion à MongoDB échouée !', err.message);
