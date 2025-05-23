6616ee6baa2f33e99deb8eeb3866dcc107312593
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-05-23 12:27:52 +0200

    feat(books): apply image compression to create & modify routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const compressImage = require('../middleware/compress-image');
 const bookCtrl = require('../controllers/book');
 const ratingCtrl = require('../controllers/rating');
 
@@ -10,8 +11,8 @@ router.get('/', bookCtrl.getAllBooks);
 router.get('/bestrating', bookCtrl.bestRating);
 router.get('/:id', bookCtrl.getOneBook);
 
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, multer, compressImage, bookCtrl.createBook);
+router.put('/:id', auth, multer, compressImage, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 router.post('/:id/rating', auth, ratingCtrl.addRating);
