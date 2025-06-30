6affa4772c7b2502306c25961e58bcdb68e45efb
Alexandre Ross-Ruggeri <alexandre.rossruggeri@gmail.com>
2025-06-30 13:03:01 +0200

    Enhance email validation in User schema
    Added lowercase conversion and regex pattern to the email field in the User schema to ensure valid email format and case-insensitive storage.

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true, lowercase: true, match: [/^\S+@\S+\.\S+$/, 'Veuillez entrer une adresse e-mail valide.'] },
   password: { type: String, required: true },
 });
 
